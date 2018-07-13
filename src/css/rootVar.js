8c9004334e14641119b092dc0eaa85b3f3591b80
Kyle <k.yasumiishi@gmail.com>
2018-07-13 13:05:41 -0700

    Refactored rootVar.js

--- src/css/rootVar.js
+++ src/css/rootVar.js
@@ -1,108 +1,51 @@
+const rootVariables = {
+  "--header-background-color": ["#292323", "#292323", "#271b12", "#5a4c50"],
+  "--editor-background-color": [
+    "#889e86",
+    "transparent",
+    "transparent",
+    "transparent"
+  ],
+  "--button-hover-color": ["#889e86", "#807ac2", "#f2cca7", "#8bbfb1"],
+  "--previewer-background-color": ["white", "white", "white", "white"],
+  "--panel-heading-background-color": [
+    "#292323",
+    "#2e315d",
+    "#271b12",
+    "#5a4c50"
+  ],
+  "--header-text-color": ["white", "white", "white", "white"],
+  "--editor-text-color": ["black", "white", "white", "black"],
+  "--border-color": ["lightgray", "lightgray", "lightgray", "lightgray"],
+  "--dropdown-text-color": ["black", "black", "black", "black"],
+  "--image-url": [
+    "none",
+    "url('https://gdurl.com/At-Y')",
+    "url('https://gdurl.com/gDik')",
+    "url('https://gdurl.com/mDXy')"
+  ]
+};
+
+function setProperty(idx) {
+  for (let property in rootVariables) {
+    if (rootVariables.hasOwnProperty(property)) {
+      const value = rootVariables[property][idx];
+      document.documentElement.style.setProperty(property, value);
+    }
+  }
+}
+
 export function selectDefault() {
-  document.documentElement.style.setProperty(
-    "--header-background-color",
-    "#292323"
-  );
-  document.documentElement.style.setProperty(
-    "--editor-background-color",
-    "#889e86"
-  );
-  document.documentElement.style.setProperty("--button-hover-color", "#889e86");
-  document.documentElement.style.setProperty(
-    "--previewer-background-color",
-    "white"
-  );
-  document.documentElement.style.setProperty(
-    "--panel-heading-background-color",
-    "#292323"
-  );
-  document.documentElement.style.setProperty("--header-text-color", "white");
-  document.documentElement.style.setProperty("--editor-text-color", "black");
-  document.documentElement.style.setProperty("--border-color", "lightgray");
-  document.documentElement.style.setProperty("--dropdown-text-color", "black");
-  document.documentElement.style.setProperty("--image-url", "none");
+  setProperty(0);
 }
 
 export function selectSpace() {
-  document.documentElement.style.setProperty(
-    "--header-background-color",
-    "#292323"
-  );
-  document.documentElement.style.setProperty(
-    "--editor-background-color",
-    "transparent"
-  );
-  document.documentElement.style.setProperty("--button-hover-color", "#807ac2");
-  document.documentElement.style.setProperty(
-    "--previewer-background-color",
-    "white"
-  );
-  document.documentElement.style.setProperty(
-    "--panel-heading-background-color",
-    "#2e315d"
-  );
-  document.documentElement.style.setProperty("--header-text-color", "white");
-  document.documentElement.style.setProperty("--editor-text-color", "white");
-  document.documentElement.style.setProperty("--border-color", "lightgray");
-  document.documentElement.style.setProperty("--dropdown-text-color", "black");
-  document.documentElement.style.setProperty(
-    "--image-url",
-    "url('https://gdurl.com/At-Y')"
-  );
+  setProperty(1);
 }
 
 export function selectDog() {
-  document.documentElement.style.setProperty(
-    "--header-background-color",
-    "#271b12"
-  );
-  document.documentElement.style.setProperty(
-    "--editor-background-color",
-    "transparent"
-  );
-  document.documentElement.style.setProperty("--button-hover-color", "#f2cca7");
-  document.documentElement.style.setProperty(
-    "--previewer-background-color",
-    "white"
-  );
-  document.documentElement.style.setProperty(
-    "--panel-heading-background-color",
-    "#271b12"
-  );
-  document.documentElement.style.setProperty("--header-text-color", "white");
-  document.documentElement.style.setProperty("--editor-text-color", "white");
-  document.documentElement.style.setProperty("--border-color", "lightgray");
-  document.documentElement.style.setProperty("--dropdown-text-color", "black");
-  document.documentElement.style.setProperty(
-    "--image-url",
-    "url('https://gdurl.com/gDik')"
-  );
+  setProperty(2);
 }
-
 export function selectRetro() {
-  document.documentElement.style.setProperty(
-    "--header-background-color",
-    "#5a4c50"
-  );
-  document.documentElement.style.setProperty(
-    "--editor-background-color",
-    "transparent"
-  );
-  document.documentElement.style.setProperty("--button-hover-color", "#8bbfb1");
-  document.documentElement.style.setProperty(
-    "--previewer-background-color",
-    "white"
-  );
-  document.documentElement.style.setProperty(
-    "--panel-heading-background-color",
-    "#5a4c50"
-  );
-  document.documentElement.style.setProperty("--header-text-color", "white");
-  document.documentElement.style.setProperty("--editor-text-color", "black");
-  document.documentElement.style.setProperty("--border-color", "lightgray");
-  document.documentElement.style.setProperty("--dropdown-text-color", "black");
-  document.documentElement.style.setProperty(
-    "--image-url",
-    "url('https://gdurl.com/mDXy')"
-  );
+  setProperty(3);
 }
